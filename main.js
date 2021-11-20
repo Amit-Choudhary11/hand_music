56871101b336877b226dbbe29637ad9696a5963a
Amit-Choudhary11 <74827051+Amit-Choudhary11@users.noreply.github.com>
2021-11-20 10:17:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-function preload(){
+mario="";
+harryPotter="";
 
+function preload(){
+mario= loadSound("smb3-overworld.mp3");
+harryPotter= loadSound("music.mp3");
 }
 
 
 function setup(){
-canvas=createCanvas(550,500);
-canvas.center();
+canvas=createCanvas(530,400);
+canvas.position(500,250);
 video=createCapture(VIDEO);
 video.hide();
+mario.play();
 }
 
 
 function draw(){
-image(video,0,0);
+image(video,0,0,530,400);
 }
